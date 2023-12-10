06304fecfb78bf2a6e722fd45b9454c50b7d7dab
Tomasz Maćkowiak <kurazu@kurazu.net>
2023-12-10 08:46:55 +0100

    day 10 scaffolding

---
+++ advent/day_10/task_1.py
@@ -0,0 +1,23 @@
+import logging
+from pathlib import Path
+from typing import TypeAlias
+
+import numpy as np
+from numpy import typing as npt
+
+from ..cli_utils import wrap_main
+from ..io_utils import get_stripped_lines
+from ..logs import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+@wrap_main
+def main(filename: Path) -> str:
+    lines = get_stripped_lines(filename)
+    return ""
+
+
+if __name__ == "__main__":
+    setup_logging()
+    main()
