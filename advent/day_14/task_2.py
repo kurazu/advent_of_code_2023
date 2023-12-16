029641df9a2297e800742c12b76b91acc0648704
Tomasz Maćkowiak <kurazu@kurazu.net>
2023-12-16 14:01:50 +0100

    WIP day 14 task 2

--- advent/day_14/task_2.py
+++ advent/day_14/task_2.py
@@ -21,17 +21,42 @@ def visualize_board(board: BoardType) -> str:
     return "\n".join("".join(inverse_map[c] for c in row) for row in board)
 
 
+def cycle(board: BoardType) -> None:
+    drop(board)
+    logger.debug("After north:\n%s", visualize_board(board))
+    drop(board.T[:, ::-1])
+    logger.debug("After west:\n%s", visualize_board(board))
+    drop(board[::-1, :])
+    logger.debug("After south:\n%s", visualize_board(board))
+    drop(board.T[::-1, :])
+    logger.debug("After east:\n%s", visualize_board(board))
+
+
 @wrap_main
 def main(filename: Path) -> str:
     lines = get_stripped_lines(filename)
     board = parse(lines)
-    logger.debug("Board:\n%s", visualize_board(board))
-    drop(board)
-    logger.debug("Dropped:\n%s", visualize_board(board))
+    logger.info("Board:\n%s", visualize_board(board))
+    memory: dict[str, int] = {
+        visualize_board(board): 0,
+    }
+    round = 0
+    while True:
+        cycle(board)
+        round += 1
+        key = visualize_board(board)
+        if key in memory:
+            logger.info("Found cycle at round %d", round)
+            break
+        memory[key] = round
+        if round % 100 == 0:
+            logger.info("Round %d", round)
+
+    # logger.info("Cycle:\n%s", visualize_board(board))
     load = get_load(board)
     return str(load)
 
 
 if __name__ == "__main__":
-    setup_logging(logging.DEBUG)
+    setup_logging(logging.INFO)
     main()
