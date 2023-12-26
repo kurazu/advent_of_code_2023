5b93b647f4de09f76c3144397e6c847f31792c12
Tomasz Maćkowiak <kurazu@kurazu.net>
2023-12-26 21:56:47 +0100

    day 25 task 1

--- advent/day_25/task_1.py
+++ advent/day_25/task_1.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Iterable, NewType, TypeAlias
 
+import networkx as nx
+
 from ..cli_utils import wrap_main
 from ..io_utils import get_stripped_lines
 from ..logs import setup_logging
@@ -12,20 +14,15 @@ Node = NewType("Node", str)
 GraphType: TypeAlias = dict[Node, set[Node]]
 
 
-def parse(lines: Iterable[str]) -> GraphType:
-    graph: GraphType = defaultdict(set)
+def parse(lines: Iterable[str]) -> nx.Graph:
+    graph = nx.Graph()
     for line in lines:
         raw_node, children_part = line.split(": ")
         node = Node(raw_node)
         children = map(Node, children_part.split(" "))
         for child in children:
-            graph[child].add(node)
-            graph[node].add(child)
+            graph.add_edge(node, child)
     return graph
-
-
-def stoer_wagner(graph: GraphType) -> None:
-    node = next(iter(graph))
 
 
 @wrap_main
@@ -33,8 +30,9 @@ def main(filename: Path) -> str:
     lines = get_stripped_lines(filename)
     graph = parse(lines)
     logger.debug("Graph: %s", graph)
-    result = stoer_wagner(graph)
-    return ""
+    cut_value, (first_partition, second_partition) = nx.stoer_wagner(graph)
+    assert cut_value == 3
+    return str(len(first_partition) * len(second_partition))
 
 
 if __name__ == "__main__":
