304e705d0d18c97d5f8d18b78d73b7fa1da2b30b
Tomasz Maćkowiak <kurazu@kurazu.net>
2023-12-26 21:33:55 +0100

    WIP day 25 task 1

--- advent/day_25/task_1.py
+++ advent/day_25/task_1.py
@@ -1,25 +1,31 @@
 import logging
 from collections import defaultdict
 from pathlib import Path
-from typing import Iterable, TypeAlias
+from typing import Iterable, NewType, TypeAlias
 
 from ..cli_utils import wrap_main
 from ..io_utils import get_stripped_lines
 from ..logs import setup_logging
 
 logger = logging.getLogger(__name__)
-GraphType: TypeAlias = dict[str, set[str]]
+Node = NewType("Node", str)
+GraphType: TypeAlias = dict[Node, set[Node]]
 
 
 def parse(lines: Iterable[str]) -> GraphType:
     graph: GraphType = defaultdict(set)
     for line in lines:
-        node, children_part = line.split(": ")
-        children = children_part.split(" ")
+        raw_node, children_part = line.split(": ")
+        node = Node(raw_node)
+        children = map(Node, children_part.split(" "))
         for child in children:
             graph[child].add(node)
             graph[node].add(child)
     return graph
+
+
+def stoer_wagner(graph: GraphType) -> None:
+    node = next(iter(graph))
 
 
 @wrap_main
@@ -27,6 +33,7 @@ def main(filename: Path) -> str:
     lines = get_stripped_lines(filename)
     graph = parse(lines)
     logger.debug("Graph: %s", graph)
+    result = stoer_wagner(graph)
     return ""
 
 
